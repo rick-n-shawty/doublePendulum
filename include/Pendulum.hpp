82823e8d7b5274482c3892ddfc25e4b587f0ce56
rick-n-shawty <sultanhoodie@gmail.com>
2024-06-26 18:02:58 +0800

    minor

--- include/Pendulum.hpp
+++ include/Pendulum.hpp
@@ -1,11 +1,15 @@
+#include <SFML/Graphics.hpp>
 #ifndef PENDULUM_HPP 
 #define PENDULUM_HPP 
 
 class Pendulum{
     private: 
-
+        sf::CircleShape ball;
+        sf::Vector2f pivot;
+        float rodLength;
+        sf::VertexArray lines; 
     public: 
-        Pendulum(); 
+        Pendulum(sf::Vector2f fixedPivot, float length, float radius); 
         ~Pendulum();
 };
 
--- src/Pendulum.cpp
+++ src/Pendulum.cpp
@@ -1,10 +1,12 @@
 #include "Pendulum.hpp"
 
 
-Pendulum::Pendulum(){
-
+Pendulum::Pendulum(sf::Vector2f fixedPivot, float length, float radius) : lines(sf::LineStrip, 2){
+    pivot = fixedPivot; 
+    rodLength = length; 
+    ball.setRadius(radius);
+    ball.setFillColor(sf::Color::White);
+    // lines.setPrimitiveType(sf::LinesStrip);
 }; 
 
-Pendulum::~Pendulum(){
-
-};
+Pendulum::~Pendulum(){};
